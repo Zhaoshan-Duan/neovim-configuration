74dc5c95e0a1ea6ebb3de6eb4cab66d639e43ec7
Joshua Duan <joshua.duan47@gmail.com>
2024-07-19 22:36:25 -0700

    add ThePrimeagen/harpoon

--- lua/jojo/init.lua
+++ lua/jojo/init.lua
@@ -24,7 +24,12 @@ local plugins = {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+    }
 }
 
 local opts = {}
