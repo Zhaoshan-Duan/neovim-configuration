eae6a6014d60de211c153205b3736d8d25329bd8
Joshua Duan <joshua.duan47@gmail.com>
2024-07-24 09:47:21 -0700

    delete alpha.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
             ensure_installed = {
                 "lua",
                 "python",
+                "javascript"
             },
             sync_install = false,
             auto_install = true,
