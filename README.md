765ba46c6b8a7f7e82b479517a5283fbd6d9e9e9
Joshua Duan <joshua.duan47@gmail.com>
2024-08-18 10:13:13 -0700

    clean up lsp-config.lua: consildate plugins together; clean up completions.lua; update README.md

--- README.md
+++ README.md
@@ -1,70 +1,89 @@
-# Neovim Configuration 
+# Neovim Configuration
+
 Theme: catppuccin
 
-## Plugins 
+## Plugins
 
 ### [treesitter](https://github.com/nvim-treesitter/nvim-treesitter)
 
 ### [telescope](https://github.com/nvim-telescope/telescope.nvim)
->Fuzzy finder
+
+> Fuzzy finder
+
 - `<leader>pf` for all file search
 - `<C-p>` for git file search
 - `<leader>ps` for live project grep
 
 ### [harpoon](https://github.com/ThePrimeagen/harpoon/tree/harpoon2)
->Quick file navigation
+
+> Quick file navigation
+
 - `<leader>a` add current file to harpoon
 - `<C-e>` toggle quick menu
 - `<C-h>`, `<C-t>`, `<C-n>`, `<C-s>` select files in the harpoon list
 
 ## [neo-tree](https://github.com/nvim-neo-tree/neo-tree.nvim)
->Browse file system
+
+> Browse file system
+
 - `<leader>n`
 
 ## [lualine](https://github.com/nvim-lualine/lualine.nvim)
->Neovim statusline
+
+> Neovim statusline
 
 ## [fugitive](https://github.com/tpope/vim-fugitive)
->Vim plugin for Git
+
+> Vim plugin for Git
+
 - `<leader>gs' git status
 
 ## [gitsigns](https://github.com/lewis6991/gitsigns.nvim)
->signs for git difference
+
+> signs for git difference
 
 ## LSP
 
-> [mason.vim](williamboman/mason.nvim) manages and installs LSPs  
+> [nvim-lspconfig](williamboman/mason-lspconfig.nvim) Core LSP configuration plugin for Neovim: connects neovim to langauge servers: set up key binding, and communication between neovim and LSPs
+
+> [mason](williamboman/mason.nvim) Pakcage manager LSP servers, DAP servers, linters and formatters.
 
 > [mason-lspconfig](williamboman/mason-lspconfig.nvim) briges gap between `mason.nvim` and `lspconfig`; ensures language servers are installed before running
 
-> [nvim-lspconfig](williamboman/mason-lspconfig.nvim) connects neovim to langauge servers: set up key binding, and communication between neovim and LSPs
+> [hrsh7th/cmp-nvim-lsp](hrsh7th/cmp-nvim-lsp) completion source for nvim-cmp that uses Neovim's built-in LSP client.
 
+> [none-ls](nvimtools/none-ls.nvim) Fork of null-ls: use Neovim as a language server to inject LSP diagnosdtics, code actions.
+
+> [jay-babu/mason-null-ls.nvim](jay-babu/mason-null-ls.nvim) bridge between `mason` and `none-ls` for easy installation of null-ls sources.
 
 LSP keybindings
+
 - `gd` go to function definition
-- `K` hover
+- `K` hover information
 - `<leader>ca` Code Action
-
-## [none-ls](https://github.com/nvimtools/none-ls.nvim)
->Linting and formatting
-
 - `<leader>gf` for formatting
 
 ## Autocomplete and Snippets
 
->[nvim.cmp](https://github.com/hrsh7th/nvim-cmp) Neovim completion engine
+> [nvim.cmp](https://github.com/hrsh7th/nvim-cmp) Neovim completion engine
 
->[LuaSnip](https://github.com/L3MON4D3/LuaSnip) Neovim snippet engine
+> [LuaSnip](https://github.com/L3MON4D3/LuaSnip) Neovim snippet engine
 
->[cmp_luasnip](https://github.com/saadparwaiz1/cmp_luasnip) luasnip completion source for nvim-cmp
+> [cmp_luasnip](https://github.com/saadparwaiz1/cmp_luasnip) luasnip completion source for nvim-cmp
 
->[Friendly Snippets](https://github.com/rafamadriz/friendly-snippets) Snippets collection for different languages
+> [Friendly Snippets](https://github.com/rafamadriz/friendly-snippets) Snippets collection for different languages
 
->[cmp-nvim-lsp](https://github.com/sar/cmp-lsp.nvim) completion source for neovim LSP
+> [cmp-nvim-lsp](https://github.com/sar/cmp-lsp.nvim) completion source for neovim LSP
 
 Completion keybindings
+
 - `<C-p> <C-n>` for selecting previous and next in completion list
 - `<C-Space>` for completion
 - `<CR>` to confirm
 - `<C-e>` for abort
-- `<C-b> <C-f>` to scroll docs 
+- `<C-b> <C-f>` to scroll docs
+
+## Tutorial Videos
+
+![Primeagen - ](https://www.youtube.com/watch?v=w7i4amO_zaE&t)
+![Typecraft - Neovim Series]()
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,100 @@
 return {
     {
-        "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "nvimtools/none-ls.nvim",
+            "jay-babu/mason-null-ls.nvim",
+        },
         config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+            local lspconfig = require("lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local none_ls = require("null-ls")
+            local mason_null_ls = require("mason-null-ls")
+
+            -- setup mason
+            mason.setup()
+
+            -- setup Mason-LSPconfig
+            mason_lspconfig.setup({
                 ensure_installed = {
-                    "tsserver",
-                    "eslint",
                     "lua_ls",
                     "tsserver",
                     "pyright",
-                    "kotlin_language_server",
+                },
+                automatic_installation = true,
+            })
+
+            mason_null_ls.setup({
+                ensure_installed = {
+                    "prettier",
+                    "eslint_d",
+                    "stylua",
+                    "black",
+                    "isort",
+                },
+                automatic_installation = true,
+            })
+
+            none_ls.setup({
+                sources = {
+                    -- lua
+                    none_ls.builtins.formatting.stylua,
+                    -- python
+                    none_ls.builtins.formatting.black,
+                    none_ls.builtins.formatting.isort,
+                    -- javascript/typescript
+                    none_ls.builtins.formatting.prettier,
+                    none_ls.builtins.diagnostics.eslint_d,
                 },
             })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
 
+            -- LSP capabilities
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            -- LSP setup
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
             })
             lspconfig.tsserver.setup({
                 capabilities = capabilities,
+                filetypes = {
+                    "typescript",
+                    "typescriptreact",
+                    "typescript.tsx",
+                    "javascript",
+                    "javascriptreact",
+                    "javascript.jsx",
+                },
+                -- Add these lines for debugging
+				on_init = function(client)
+					client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+					return true
+				end,
+				on_attach = function(client, bufnr)
+					vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+					print("TSServer attached!")
+				end,
+				flags = {
+					debounce_text_changes = 150,
+				},
             })
+
             lspconfig.pyright.setup({
                 capabilities = capabilities,
             })
-            lspconfig.kotlin_language_server.setup({
-                capabilities = capabilities,
-            })
 
-            --  local opts = {buffer = bufner, remap = false}
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            --            vim.keymap.set('n', '[d', vim.diagnostic.goto_next, {})
-            --            vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            --global keybindings
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover information" })
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
+
+            vim.lsp.set_log_level("debug")
         end,
     },
 }
